e0c57655ccd231e24a853f049e34b87979e355af
Duane <duanerichard47@hotmail.com>
2023-01-11 21:26:25 -0500

    completed phase 1 scope lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var bestCustomer;
+var customerName = 'bob';
+const leastFavoriteCustomer = 'blank';
+
+function upperCaseCustomerName() {
+    customerName= customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'new least';
+}
+
